644dfd9f740229473cc688c244c03250953052fe
Jennifer Christos <jennifer.christos3@gmail.com>
2014-12-14 15:54:37 -0500

    Animate squares diagonally

--- js/pendulum.js
+++ js/pendulum.js
@@ -1,3 +1,4 @@
+var interval;
 function canvas() {
   var context = myCanvas.getContext('2d');
   var x = 0, y = 0, width = myCanvas.width, height = myCanvas.height;
@@ -7,17 +8,23 @@ function canvas() {
   //   context.fillRect(x, y, width, height);
   // }
   function drawManyRectangles() {
-    var i = 0;
+    // var i = 0;
     var n = 10;
     width = 20;
     height = 20;
-    while (i < n) {
-      x = i/10 * myCanvas.width;
-      y = i/10 * myCanvas.height;
-      context.fillRect(x, y, width, height);
-      //console.log(i);
-      i ++; // same as `i = i+1` and `i +=1`
+    var time = 0;
+    var redraw = function () {
+      context.clearRect(0, 0, myCanvas.width, myCanvas.height);
+      for (var i=0; i < n; i++) {// while (i < n) {
+        x = i/10 * myCanvas.width + time;
+        y = i/10 * myCanvas.height + time;
+        context.fillRect(x, y, width, height);
+        //console.log(i);
+        // i ++; // same as `i = i+1` and `i +=1`
+      }
+      time ++;
     }
+    interval = setInterval(redraw, 20);
   }
   drawManyRectangles();
 }
