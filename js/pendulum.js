45ab0cecf349f3a18fd481b6019be5a6830eaf49
Jennifer Christos <jennifer.christos3@gmail.com>
2014-12-14 16:09:04 -0500

    Particle man

--- js/pendulum.js
+++ js/pendulum.js
@@ -14,19 +14,21 @@ function canvas() {
   // }
   function drawManyRectangles() {
     // var i = 0;
-    var n = 20;
-    width = 20;
-    height = 5;
+    var n = 100;
+    width = 2;
+    height = 2;
     var time = 0;
     var redraw = function () {
       context.clearRect(0, 0, myCanvas.width, myCanvas.height);
       for (var i=0; i < n; i++) {// while (i < n) {
+        x = myCanvas.width / 2 + Math.sin(time * i) * 20;
+        y = i/n * myCanvas.height;
         // x = myCanvas.width / 2 + Math.sin(time * i) * 20; // 2 + time; // i/10 * myCanvas.width + time;
         // y = myCanvas.width / 2 + Math.cos(time * i) * 20;// y = i/n * myCanvas.height; // i/10 * myCanvas.height + time;
-        var sin = Math.sin(i / 20 * time) / 2 + 0.5;
-        var cos = Math.cos(i / 20 * time) / 2 + 0.5;
-        x = sin * (myCanvas.width - width);
-        y = cos * (myCanvas.height - height);
+        // var sin = Math.sin(i / 20 * time) / 2 + 0.5;
+        // var cos = Math.cos(i / 20 * time) / 2 + 0.5;
+        // x = sin * (myCanvas.width - width);
+        // y = cos * (myCanvas.height - height);
         context.fillRect(x, y, width, height);
         //console.log(i);
         // i ++; // same as `i = i+1` and `i +=1`
